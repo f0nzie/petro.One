d08a3c199e16aeb8d02622ba58b813f400a37193
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2018-04-24 19:50:51 -0500

    add ... to read_multidoc() add verbose param to read_multipage(). add verbose cat().

--- R/multipage.R
+++ R/multipage.R
@@ -1,4 +1,4 @@
-
+# these document types have a different structure
 do_not_read <- c("media", "standard", "other")
 
 #' @title Read all OnePetro papers metadata by type of document
@@ -6,7 +6,7 @@ do_not_read <- c("media", "standard", "other")
 #' papers into a common dataframe
 #' @param my_url OnePetro query URL
 #' @export
-read_multidoc <- function(my_url) {
+read_multidoc <- function(my_url, ...) {
 
     sdc_df <- papers_by_type(my_url)
 
@@ -20,7 +20,7 @@ read_multidoc <- function(my_url) {
     for (doc in sdc_df$name) {
         if (!any(grepl(doc, do_not_read, ignore.case = TRUE))) {
             my_url <- set_doctype(my_url, doc)
-            df <- read_multipage(my_url)
+            df <- read_multipage(my_url, ...)
             cum_df <- rbind(cum_df, df)
         }
     }
@@ -37,7 +37,7 @@ read_multidoc <- function(my_url) {
 #' @param doctype a OnePetro paper type: conference-paper, journal-paper, general.
 #' presentation, chapter, etc.
 #' @export
-read_multipage <- function(url, doctype = NULL) {
+read_multipage <- function(url, doctype = NULL, verbose = FALSE) {
     df_cum <- data.frame()
     doc <- urltools::param_get(url, "dc_type")
     if (is.null(doctype) && is.na (doc)) stop("must provide paper type")
@@ -54,9 +54,9 @@ read_multipage <- function(url, doctype = NULL) {
     } else {
         # read page by page in thousands size
         paper_count <- get_papers_count(url)
-
         # cat("paper_count:", paper_count, "\n")
         pages <- paper_count %/% 1000 + ifelse((paper_count %% 1000) > 0, 1, 0)
+
 
         for (page in 1:pages) {
             url <- urltools::param_set(url, "start", 1000 * page - 1000)
@@ -65,6 +65,7 @@ read_multipage <- function(url, doctype = NULL) {
             df <- onepetro_page_to_dataframe(url)
             # print(df[1, ])   # print first row of dataframe
             df_cum <- rbind(df_cum, df)  # accumulate dataframes
+            if (verbose) cat(sprintf("%3d %3d %s %s \n", paper_count, pages, doc, url))
         }
     }
     df_cum
