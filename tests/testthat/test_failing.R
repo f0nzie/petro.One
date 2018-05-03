50d2b10b0407caabdea1528dca6af43e0c545349
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2018-05-04 02:32:28 +0800

    ad failing tests to unit test

---
+++ tests/testthat/test_failing.R
@@ -0,0 +1,33 @@
+library(testthat)
+
+test_that("deepwater, other return GTE 12", {
+    my_url <- make_search_url(query = "deepwater",
+                              dc_type = "other")
+    expect_true(get_papers_count(my_url) >= 12)
+    # 12
+    expect_true(dim(onepetro_page_to_dataframe(my_url))[1] >=12)
+})
+
+
+test_that("deepwater, media return GTE 69", {
+    my_url <- make_search_url(query = "deepwater", dc_type = 'media')
+
+    expect_true(get_papers_count(my_url) >= 69)
+    # 69
+    expect_true(dim(onepetro_page_to_dataframe(my_url)) >= 69)
+})
+
+
+test_that("shale oil, all, conference-paper, works with rows > 1000", {
+    my_url <- make_search_url(query = "shale oil",
+                              how = "all",
+                              dc_type = "conference-paper",
+                              rows = 1100)
+
+    expect_true(get_papers_count(my_url) >= 2262)
+    expect_true(papers_by_type(my_url)$value >= 2262)
+    # 2578, dc_type = NULL
+    # 2578, dc_type = "conference-paper"
+    #  380, dc_type = "journal-paper"
+    print(dim(onepetro_page_to_dataframe(my_url)))
+})
