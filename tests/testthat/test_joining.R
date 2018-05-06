d1fffe6f32dcdb88a67d3abb8cf097cf6390508a
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2018-05-07 05:15:26 +0800

    add comments

--- tests/testthat/test_joining.R
+++ tests/testthat/test_joining.R
@@ -14,9 +14,9 @@ test_that("both dataframes return rows - test #5", {
                           sleep = 2, verbose = FALSE)
 
     expect_true(dim(p.df$keywords)[1] == 2)         # number of rows
+    expect_true(nrow(p.df$papers)[1] == 26)
     expect_true(p.df$keywords$paper_count[1] == 15)
     expect_true(p.df$keywords$paper_count[2] == 11)
-    expect_true(nrow(p.df$papers)[1] == 26)
 })
 
 
@@ -30,12 +30,14 @@ test_that("both dataframes return rows - test #3", {
     p.df <- join_keywords(major, minor, lesser, get_papers = TRUE,
                           sleep = 2, verbose = FALSE)
 
+    expect_true(dim(p.df$keywords)[1] == 4)   # number of rows
+    expect_true(dim(p.df$papers)[1] == 460)
+    # comparing paper count
     expect_equal(p.df$keywords$paper_count[1], 115)
     expect_equal(p.df$keywords$paper_count[2], 115)
     expect_equal(p.df$keywords$paper_count[3], 115)
     expect_equal(p.df$keywords$paper_count[4], 115)
     expect_equal(p.df$keywords$paper_count[1], p.df$keywords$paper_count[2])
     expect_equal(p.df$keywords$paper_count[3], p.df$keywords$paper_count[4])
-    expect_true(dim(p.df$keywords)[1] == 4)   # number of rows
-    expect_true(dim(p.df$papers)[1] == 460)
+
 })
