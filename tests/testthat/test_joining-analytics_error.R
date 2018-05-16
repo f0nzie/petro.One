46a04f1d2aa2cf276d810a6e97af354a01418d32
petroni0 macosx <alfonso.reyes@aemenersol.com>
2018-05-16 08:30:17 +0800

    update test from 14718 papers to 14720

--- tests/testthat/test_joining-analytics_error.R
+++ tests/testthat/test_joining-analytics_error.R
@@ -1,12 +1,15 @@
 library(testthat)
-library(petro.One)
+#library(petro.One)
+
+context("14000+ papers, join_keywords() on *analytics*")
 
 major_02 <- c("analytics", "data analytics")
 
 # the returning data structure is a a list
 # the list contains two dataframes: one for the keywords and a second for the papers
-major_02_res <- join_keywords(major_02, get_papers = TRUE, sleep = 3, verbose = TRUE)
-major_02_res
+major_02_res <- join_keywords(major_02, get_papers = TRUE,
+                              sleep = 3,
+                              verbose = TRUE)
 # 1  1901 'data+driven'
 # 2  1901 'data-driven'
 
@@ -28,7 +31,7 @@ major_02_res
 # test #1
 url <- make_search_url("analytics", how = "all")
 webpage <- xml2::read_html(url)
-expect_equal(get_papers_count(url), 14718)
+expect_equal(get_papers_count(url), 14720)
 
 # test #2
 expect_equal(dim(petro.One:::get_dc_type_raw(webpage)), c(10, 7))
