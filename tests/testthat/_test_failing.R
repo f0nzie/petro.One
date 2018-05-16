082782a1ee9b3867ecf08777fc27a3bf385878fa
petroni0 macosx <alfonso.reyes@aemenersol.com>
2018-05-16 08:29:02 +0800

    rename tests to catch failed test

--- tests/testthat/_test_failing.R
+++ tests/testthat/_test_failing.R
@@ -23,15 +23,15 @@ test_that("deepwater, media return GTE 69", {
 })
 
 # skip("skip")
-context("Fails at shale oil, all, conference-paper, with rows > 1000")
-
-test_that("shale oil, all, conference-paper, works with rows > 1000", {
-    my_url <- make_search_url(query = "shale oil",
-                              how = "all",
-                              dc_type = "conference-paper",
-                              rows = 1100)
-    cat("\t", get_papers_count(my_url), "\n")
-    expect_true(get_papers_count(my_url) == 2775)
-    cat("\t", papers_by_type(my_url)$value, "\n")
-    expect_equal(papers_by_type(my_url)$value, c(2262, 20, 425, 8, 47, 13))
-})
+# context("Fails at shale oil, all, conference-paper, with rows > 1000")
+#
+# test_that("shale oil, all, conference-paper, works with rows > 1000", {
+#     my_url <- make_search_url(query = "shale oil",
+#                               how = "all",
+#                               dc_type = "conference-paper",
+#                               rows = 1100)
+#     # cat("\t", get_papers_count(my_url), "\n")
+#     expect_true(get_papers_count(my_url) == 2775)
+#     # cat("\t", papers_by_type(my_url)$value, "\n")
+#     # expect_equal(papers_by_type(my_url)$value, c(2262, 20, 425, 8, 47, 13))
+# })
