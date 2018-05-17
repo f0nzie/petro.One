01860a6fc177bb79f6a1017acd9d753ab6695823
f0nzie zbook <alfonso.reyes@oilgainsanalytics.com>
2018-05-17 08:50:05 +0800

    add test_that. do not use return() in function

--- tests/testthat/test-remove_duplicates_by.R
+++ tests/testthat/test-remove_duplicates_by.R
@@ -2,6 +2,8 @@ library(testthat)
 
 skip_on_travis()
 
+context("remove duplicates drilling and data driven papers")
+
 major <- c("data driven")
 minor <- c("drilling")
 
@@ -9,10 +11,15 @@ minor <- c("drilling")
 # the list contains two dataframes: one for the keywords and a second for the papers
 dd_drilling <- join_keywords(major, minor, get_papers = TRUE, sleep = 3,
                              verbose = FALSE)
-context("drilling and data driven papers by paper_id")
-result <- remove_duplicates_by(dd_drilling$papers, by ="paper_id" )
-expect_equal(dim(result), c(788, 7))
+test_that("by paper_id", {
+    result <- remove_duplicates_by(dd_drilling$papers, by ="paper_id" )
+    expect_equal(dim(result), c(788, 7))
+})
 
-context("drilling and data driven papers by book_title")
-result <- remove_duplicates_by(dd_drilling$papers, by ="book_title" )
-expect_equal(dim(result), c(779, 7))
+
+# context("drilling and data driven papers by book_title")
+test_that("by book title", {
+    result <- remove_duplicates_by(dd_drilling$papers, by ="book_title" )
+    expect_equal(dim(result), c(779, 7))
+})
+
