a030f5f993fd25ce48c90f80aa7a8ea36607aa20
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2019-01-10 07:55:09 -0600

    change from expect_gte to expect_equal. change expected values

--- tests/testthat/test_summary_by_group.R
+++ tests/testthat/test_summary_by_group.R
@@ -1,11 +1,17 @@
 library(testthat)
 
+skip_on_cran()
+skip_on_travis()
+
 context("new summaries: papers_by_type")
 
-my_url <- make_search_url(query = "mechanistic performance",
-                          how = "all")
-df <- papers_by_type(my_url)
-expect_gte(sum(df$value), 4)
+test_that("mechanistic performance match papers_by_type()", {
+    my_url <- make_search_url(query = "mechanistic performance",
+                              how = "all")
+    df <- papers_by_type(my_url)
+    expect_equal(sum(df$value), 4)
+})
+
 
 
 
@@ -14,31 +20,31 @@ context("new summaries: papers_by_publisher")
 my_url <- make_search_url(query = "mechanistic performance",
                           how = "all")
 df <- papers_by_publisher(my_url)
-expect_gte(sum(df$value), 4)
+expect_equal(sum(df$value), 4)
 
 
 my_url <- make_search_url(query = "mechanistic",
                           how = "all")
 df <- papers_by_publisher(my_url)
-expect_gte(sum(df$value), 4938)
+expect_equal(sum(df$value), 5410)
 
 
 my_url <- make_search_url(query = "IPR",
                           how = "all")
 df <- papers_by_publisher(my_url)
-expect_gte(sum(df$value), 1844)
+expect_equal(sum(df$value), 1979)
 
 
 my_url <- make_search_url(query = "BHP",
                           how = "all")
 df <- papers_by_publisher(my_url)
-expect_gte(sum(df$value), 7890)
+expect_equal(sum(df$value), 8485)
 
 
 my_url <- make_search_url(query = "shale gas",
                           how = "all")
 df <- papers_by_publisher(my_url)
-expect_gte(sum(df$value), 7598)
+expect_equal(sum(df$value), 8600)
 
 
 
@@ -47,25 +53,25 @@ context("papers_by_year")
 my_url <- make_search_url(query = "shale gas",
                           how = "all")
 df <- papers_by_year(my_url)
-expect_gte(sum(df$value), 479899)
+expect_equal(sum(df$value), 549915)
 
 
 my_url <- make_search_url(query = "production automation",
                           how = "all")
 df <- papers_by_year(my_url)
-expect_gte(sum(df$value), 2275)
+expect_equal(sum(df$value), 2437)
 
 
 my_url <- make_search_url(query = "mechanistic performance",
                           how = "all")
 df <- papers_by_year(my_url)
-expect_gte(sum(df$value), 10)
+expect_equal(sum(df$value), 10)
 
 
 my_url <- make_search_url(query = "mechanistic performance",
                           how = "any")
 df <- papers_by_year(my_url)
-expect_gte(sum(df$value), 150878)
+expect_equal(sum(df$value), 170983)
 
 
 
@@ -75,9 +81,9 @@ context("papers_by_publication")
 my_url <- make_search_url(query = "mechanistic performance",
                           how = "all")
 df <- papers_by_publication(my_url)
-expect_gte(sum(df$value), 4)
+expect_equal(sum(df$value), 4)
 
 my_url <- make_search_url(query = "mechanistic performance",
                           how = "any")
 df <- papers_by_publication(my_url)
-expect_gte(sum(df$value), 3276)
+expect_equal(sum(df$value), 3623)
