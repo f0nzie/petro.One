aea52fec312cffa431e8269d3ee33da423ce754c
alfonso zbook <alfonso.r.reyes@gmail.com>
2019-01-11 20:11:51 -0600

    rename to _tests so we don't run while testing the vignettes

--- tests/testthat.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-library(petro.One)
-
-# test_check("petro.One") #, stop_on_failure = FALSE)
-
-# test_check("petro.One", stop_on_failure = FALSE, reporter = "ProgressReporter")
-# test_file("testthat/test_failing.R")
-# test_file("testthat/test_joining.R")
